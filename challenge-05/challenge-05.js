7c4739a363dee4c7425697a618dd5a8a4815f475
Denis <djr_08rj@hotmail.com>
2025-03-10 01:23:44 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [ 'Denis' , 'Lopes' , 'Gonçalves' , 'Junior', 'Caveira' ];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction(arr) {
+  return arr;
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+myFunction(arr)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,20 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFunction2(arr , b) {
+  return arr[b];
+}; 
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var difer = [ 23 , 'Denis' , null , true , {} ];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+myFunction2(difer);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,8 +51,14 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
-
+function book(nome) {
+  var prop = { 
+    Matança: { quantidadePaginas: 168 , autor: 'Jessedy P.' , editora: 'Rumons' },
+    Julgamento: { quantidadePaginas: 97 , autor: 'Erick C.' , editora: 'Police' },
+    Pequeno: { quantidadePaginas: 19 , autor: 'Jubileu' , editora: 'Fox' }
+             };
+  return prop.nome;
+};
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
