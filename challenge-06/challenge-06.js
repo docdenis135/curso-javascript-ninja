7d648668ca49696fb4412eb458e59b177095f6f2
Denis <djr_08rj@hotmail.com>
2025-03-10 14:27:57 -0300

    Update challenge-06.js

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,7 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Campeonato Carioca';
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,8 +12,13 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
-
+var teams = [
+  'Fluminense',
+  'Bangu',
+  'Flamengo',
+  'Vasco',
+  'Botafogo'
+  ]
 console.log( 'Times que estão participando do campeonato:', teams );
 
 /*
@@ -32,7 +37,14 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition(x) {
+  var pos = teams[x - 1];
+  if (x > 5) {
+    return 'Não temos informação do time que está nessa posição.';
+  } else {
+  return ' O time que está em ' + x + 'º lugar é o ' + pos;
+}
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
